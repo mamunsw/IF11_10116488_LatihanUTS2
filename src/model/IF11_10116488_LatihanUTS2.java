dcfcb82af2707c161bcc752c77d1e8a8049c86ed
MSW98 <MSW98@DESKTOP-4BCNEIF>
2017-11-14 20:14:55 +0700

    IF11_10116488_LatihanUTS2

---
+++ src/model/IF11_10116488_LatihanUTS2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import view.gajiPgw;
+
+/**
+ *
+ * @author MSW98
+ */
+public class IF11_10116488_LatihanUTS2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new gajiPgw().setVisible(true);
+    }
+    
+}
